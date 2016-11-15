fb3e8cfc5a9173235f8c195c36af56024dd2691c
Vicente Esteche <vcaes88@gmail.com>
2016-11-14 21:23:55 -0300

    Delete 20160928214629_create_facturas_servicios.rb

--- db/migrate/20160928214629_create_facturas_servicios.rb
+++
@@ -1,8 +0,0 @@
-class CreateFacturasServicios < ActiveRecord::Migration[5.0]
-  def change
-	create_table :facturas_servicios, id: false do |t|
-      t.belongs_to :factura, index: true
-      t.belongs_to :servicio, index: true
-    end
-  end
-end
